59673f3426b9542460621451ef4bbecbcae18a1b
iSepiroth <catan.aris@gmail.com>
2012-08-16 01:46:45 +0800

    add item_conversion_details table dump

---
+++ STG_Milling/dump file/tables dump/item_conversion_details.sql
@@ -0,0 +1,6 @@
+﻿DROP TABLE IF EXISTS `dbinventory`.`item_conversion_details`;
+CREATE TABLE  `dbinventory`.`item_conversion_details` (
+  `parent_id` varchar(45) DEFAULT NULL,
+  `associated_id` varchar(45) DEFAULT NULL,
+  `qty` int(10) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
